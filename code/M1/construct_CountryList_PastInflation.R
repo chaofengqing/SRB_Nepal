f927b63b2098e11a3c290873cf892a3721a5790c
qiqi <108138869+loofla@users.noreply.github.com>
2025-08-01 10:01:41 +0800

    Update construct_CountryList_PastInflation.R
    Only delete commented on code

--- code/M1/construct_CountryList_PastInflation.R
+++ code/M1/construct_CountryList_PastInflation.R
@@ -10,20 +10,9 @@ cutoff <- 0.95
 count <- 0
 for (j in 1:C.adj) {
   c <- c.adj[j]
-  # t.datastart <- min(t.i[j.i == j]) #start of data period
-  # t.dataend <- max(t.i[j.i == j]) #end of data period
-  # 
-  # alpha.lt <- matrix(0, L, Tend)
-  # 
-  # for (t in which(years.t == adj.year):Tend) {
-  #   alpha.lt[, t] <- c(mcmc.array[, , paste0("alpha.jt[", j, ",", t, "]")])
-  # }# end of t loop
-
+  
   delta.l <- c(mcmc.array[, , paste0("delta.j[", j, "]")])
   
-  # zero.alpha.dataperiod <- apply(alpha.lt[, t.datastart:t.dataend] == 0, 1, prod) == 1
-
-  # inflation.prob.j[j] <- mean(delta.l == 1 & !zero.alpha.dataperiod)
   inflation.prob.j[j] <- mean(delta.l == 1)
   
   if (inflation.prob.j[j] >= cutoff) {
@@ -34,7 +23,6 @@ for (j in 1:C.adj) {
   
 }#end of j loop
 
-# save(country.list.past.inflation, file = paste0(output.dir, "country.list.past.inflation.rda"))
 cat("Number of areas with past inflation:", length(country.list.past.inflation), "\n")
 print(sort(round(inflation.prob.j[!is.element(name.c[c.adj],
                                               country.list.past.inflation)]*100, 1), decreasing = TRUE))
