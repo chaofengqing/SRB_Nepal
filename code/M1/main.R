8ac93db6c8f0722fbf64e7d8bd5b8c5699b83d18
qiqi <108138869+loofla@users.noreply.github.com>
2025-08-07 21:57:37 +0800

    Update main.R
    unfinished

--- code/M1/main.R
+++ code/M1/main.R
@@ -1,4 +1,9 @@
-
+# Estimation and probabilistic projection of levels and trends 
+# in the sex ratio at birth in seven provinces of Nepal
+# from 1980 to 2050: a Bayesian modeling approach
+#
+# Code constructed by: Fengqing CHAO
+# Code last revised by: Qiqi Qiang on 7 Aug 2025
 
 ## Master code file ##
 ## This run is to get normal level of SRB only!!!
@@ -42,7 +47,7 @@ source(paste0("code/", runname, "/source_DataSetup.R"))
 ## data for adjustment factor ##
 source(paste0("code/", runname, "/source_adj_DataSetup.R"))
 ## data related to TFR ##
-source(paste0("code/", runname, "/source_ADJ&TFR-relation.R"))
+source(paste0("code/", runname, "/source_ADJ&TFR_relation.R"))
 # setup MCMC settings
 source(paste0("code/", runname, "/jags_setupMCMC.R"))
 
