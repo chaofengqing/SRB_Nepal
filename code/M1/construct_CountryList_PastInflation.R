0db4fc603abcce15e86bc62606d1f469309c6681
qiqi <108138869+loofla@users.noreply.github.com>
2025-08-07 13:31:29 +0800

    Update construct_CountryList_PastInflation.R
    add comments

--- code/M1/construct_CountryList_PastInflation.R
+++ code/M1/construct_CountryList_PastInflation.R
@@ -1,5 +1,26 @@
-
-
+# Estimation and probabilistic projection of levels and trends 
+# in the sex ratio at birth in seven provinces of Nepal
+# from 1980 to 2050: a Bayesian modeling approach
+#
+# Code constructed by: Fengqing CHAO
+# Code last revised by: Qiqi Qiang on 7 Aug 2025
+# construct_CountryList_PastInflation.R
+# 
+# This script identifies countries with past SRB inflation 
+#
+# used for which run: Main.run
+#
+# this script is called by any other scripts: main_output.R
+#
+# this script calls other scripts: null
+#
+# functions called: null
+# 
+# input data: null
+#
+# output data: null
+#
+###############################################################################
 ## identify countries with past SRB inflation ##
 country.list.past.inflation <- NULL
 inflation.prob.j <- rep(NA, C.adj)
@@ -19,9 +40,9 @@ for (j in 1:C.adj) {
     count <- count + 1
     cat(count, name.c[c], inflation.prob.j[j], "\n")
     country.list.past.inflation <- c(country.list.past.inflation, name.c[c])
-  }#end of if(mean(delta.l == 1) == 1)
+  } # end of if(mean(delta.l == 1) == 1)
   
-}#end of j loop
+} # end of j loop
 
 cat("Number of areas with past inflation:", length(country.list.past.inflation), "\n")
 print(sort(round(inflation.prob.j[!is.element(name.c[c.adj],
