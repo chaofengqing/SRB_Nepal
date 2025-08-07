829b4ed668e4133183a8984ad65480749a7dfe29
qiqi <108138869+loofla@users.noreply.github.com>
2025-08-07 22:00:24 +0800

    Update main.R
    add comments

--- code/M1/main.R
+++ code/M1/main.R
@@ -4,6 +4,33 @@
 #
 # Code constructed by: Fengqing CHAO
 # Code last revised by: Qiqi Qiang on 7 Aug 2025
+# main.R
+# 
+# This script is the master file to get MCMC array for Main.run. Run this
+# script to get reulsts.
+#
+# used for which run: main.run
+#
+# this script is called by any other scripts: null
+#
+# this script calls other scripts:
+# 1. source_BasicSetup.R
+# 2. source_DirectorySetup.R
+# 3. source_DataSetup.R - is only called once and is in this script.
+# 4. source_adj_DataSetup.R
+# 5. source_ADJ&TFR_relation.R
+# 6. jags_setupMCMC.R
+# 7. jags_getMCMC.R
+#
+# functions called: Null
+# 
+#
+# output data in folder data/:
+# 1. interim/database_for_modeling_2020-05-11.csv - the SR database for modelling
+# 2. output/M1/temp.JAGSobjects/  - stepwise JAGS trajectory output
+# Note: only the main output data are listed here since it is a master script.
+#
+###############################################################################
 
 ## Master code file ##
 ## This run is to get normal level of SRB only!!!
