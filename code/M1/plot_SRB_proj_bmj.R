db191a24441ea074f2431d5b0123a4df0996f27a
qiqi <108138869+loofla@users.noreply.github.com>
2025-08-07 23:44:59 +0800

    Update plot_SRB_proj_bmj.R
    add comments

--- code/M1/plot_SRB_proj_bmj.R
+++ code/M1/plot_SRB_proj_bmj.R
@@ -8,8 +8,23 @@
 #
 # plot_CI_SRB_NepalProvince.R
 #
+# This script plots projected SRB with uncertainty and baseline for 
+# each province of Nepal.
 #
+# used for which run: Main.run
 #
+# this script is called by any other scripts: main_output.R;
+#
+# this script calls other scripts: null
+#
+# functions called: function(2) means the function is called twice in this
+# script. Those functions called in the scripts listed above are not listed.
+# PlotCIbandwithDataseries(1)
+#
+# input data: null
+#
+# output data in folder fig/: 
+# 1. CIs_SRB_Nepal_province_M1_bmj.pdf 
 ###############################################################################
 pdf(paste0(fig.dir, "CIs_SRB_Nepal_province_", runname, "_bmj.pdf"),
     height = 50, width = 25)
@@ -28,8 +43,8 @@ for (country in name.c) {
   PlotCIbandwithDataseries(
     if.LogScale = TRUE, if.SurveyLegend = TRUE,
     year.t = floor(years.t),
-    CI1s = R.qt, #nameCI1 = ifelse(c == 1, "Estimated SRB", NA),
-    CI2s = a.qt, #nameCI2 = ifelse(c == 1, "National SRB baseline", NA),
+    CI1s = R.qt, 
+    CI2s = a.qt,
     x = floor(years.t), select.x = TRUE, x.lim = c(2010, 2050),
     SElim = plot.range, datalim = plot.range,
     main = country, #alpha.dataseries = 0.8, cex.dataseries = 1.5,
@@ -39,14 +54,14 @@ for (country in name.c) {
   if (c == 1) {
     legend("topright", c("Provincial SRB", "National SRB baseline"),
            col = c("red", "darkgreen"), lwd = 10, cex = text.cex)
-  }#end of if(c == 1)
+  } # end of if(c == 1)
   axis(1, at = 2016, labels = 2016)
   abline(v = 2016)
   
   max.yr <- years.t[which.max(R.qt[2, ])]-0.5
   abline(v = max.yr, lty = 3, col = "red3")
   text(pos = 4, x = max.yr, y = 1.125, labels = max.yr, col = "red3", cex = text.cex)
-}#end of c loop
+} # end of c loop
 dev.off()
 
 for (country in name.c) {
