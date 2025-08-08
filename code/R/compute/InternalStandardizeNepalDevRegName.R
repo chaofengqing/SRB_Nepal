1aedda7e32ebcb9baf9fa3820ffd0cdebfdc6582
qiqi <108138869+loofla@users.noreply.github.com>
2025-08-08 23:41:42 +0800

    Update InternalStandardizeNepalDevRegName.R
    add comments

--- code/R/compute/InternalStandardizeNepalDevRegName.R
+++ code/R/compute/InternalStandardizeNepalDevRegName.R
@@ -1,4 +1,19 @@
-
+###############################################################################
+# Estimation and probabilistic projection of levels and trends 
+# in the sex ratio at birth in seven provinces of Nepal
+# from 1980 to 2050: a Bayesian modeling approach
+#
+# Code constructed by: Fengqing CHAO
+# Code last revised by: Qiqi Qiang on 8 Aug 2025
+# 
+# InternalStandardizeNepalDevRegName.R
+# 
+# This script contains all functions related to standardizing Nepal's region name.
+# Functions are: function1(.., function2(3), ..); means function2 is called
+# three times inside function1.
+# InternalStandardizeNepalDevRegName(..)
+###############################################################################
+#------------------------------------------------------------------------------
 
 InternalStandardizeNepalDevRegName <- function(name.in) {
   
@@ -10,4 +25,4 @@ InternalStandardizeNepalDevRegName <- function(name.in) {
   
   name.out <- name.in
   return(name.out)
-}#end of InternalStandardizeNepalDevRegName function
+} # end of InternalStandardizeNepalDevRegName function
